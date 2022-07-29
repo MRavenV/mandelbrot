ecbe7f136bd5bc0de6ba172293a275f0315af646
Mikhail Voronov <mikhail.voronov@itslearning.com>
2022-07-29 15:39:17 +0200

    create first picture (isn't compilable yet)

--- Mnd.Cmd/Program.fs
+++ Mnd.Cmd/Program.fs
@@ -1,2 +1,49 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+open Mnd.Core
+
+printfn "Mandelbrot Generator"
+
+open Mnd.Core.Util
+open Mnd.Core.Render
+
+let fname = "out.png"
+let width = 800
+let height = 600
+let screenSize = (width, height)
+
+let cameraPosition = (0.0, 0.0)
+let rotationAngle = 0.0
+let frameHeight = 1.0
+let cameraScale = frameHeight   // The same?
+
+let maxIterations = 100
+let subsampleCells = 1
+let escapeRadius = 2000.0
+let cameraRotation = degToRad rotationAngle
+
+
+let inverseViewProj =
+    inverseProject (toFloatTuple2 screenSize)
+    >> inverseView cameraPosition cameraRotation cameraScale
+    
+let coreShader = Shaders.cycleMandelbrotSmooth escapeRadius maxIterations
+let screenShader = inverseViewProj >> coreShader
+
+let aaShader = wrapAntiAliasing subsampleCells screenShader
+
+let finalShader =
+    toFloatTuple2
+    >> Vector2.add (0.5, 0.5)
+    >> aaShader
+
+printfn "Rendering frame..."
+let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+let bmp = render finalShader screenSize
+stopWatch.Stop()
+printfn "Render complete."
+printfn "Elapsed time: %f seconds." stopWatch.Elapsed.TotalSeconds
+
+printfn "Saving to: %s" fname
+bmp.Save(fname)
+printfn "Render saved."
---
+++ Mnd.Core/Render.fs
@@ -0,0 +1,57 @@
+﻿namespace Mnd.Core
+
+open Mnd.Core.Color
+open Mnd.Core.Util
+open System.Drawing
+
+module Render =
+    let render (f:int*int->RgbColor) (width:int, height:int) =
+        let bmp = new Bitmap(width, height)
+        let coords = seq {
+            for i in 0..height-1 do
+                for j in 0..width-1 do
+                    yield (j,i)
+        }
+
+        let coordFunc = indexToCoord width
+
+        let indexShader = coordFunc >> f >> RgbColor.map floatColorToScreen
+
+        let pixels =
+            Array.Parallel.init (width * height) indexShader
+            |> Dithering.dither width
+
+        let writeFunc idx color =
+            let (x, y) = coordFunc idx
+            bmp.SetPixel(x, y, (RgbColor.RawToGdiColor color))
+
+        Array.iteri writeFunc pixels
+        bmp
+        
+    let subSample cells i =
+        let interval = 1.0 / (float cells)
+        let halfInterval = interval/2.0
+        seq { (i - 0.5 + halfInterval)..interval..(i + 0.5 - halfInterval) }
+
+    let wrapAntiAliasing cells f (x, y) =
+        let samp = subSample cells
+
+        // INITIAL LINE: let cols: seq<float> = seq {
+        let cols: seq<float> = seq {
+            for dy in (samp y) do
+                for dx in (samp x) do
+                    yield f (dx, dy) }
+
+        Seq.average cols
+
+    let inverseProject (screenWidth, screenHeight) =
+        let aspectRatio = screenWidth / screenHeight
+
+        Vector2.mul (1.0/screenWidth, 1.0/screenHeight)
+        >> Vector2.add (-0.5, -0.5)
+        >> Vector2.mul (2.0, -2.0)
+        >> Vector2.mul (aspectRatio, 1.0)
+
+    let inverseView cameraPosition rotation scale =
+        Vector2.scale scale >> Vector2.rotate rotation >> Vector2.add cameraPosition
+
